adbc18bb26684d2f953f835c4ef74a384b027238
Roy Ratcliffe <royratcliffe@me.com>
2022-03-02 19:32:15 +0000

    Shield update by Linux runner (#2)

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # GitHub action for SWI-Prolog pack installation with test coverage
 
+Why is this GitHub action repository useful? You can use it to perform
+continuous development and integration on GitHub while running tests
+simultaneously against macOS, Windows and Ubuntu Linux operating systems
+across the latest stable and development versions of SWI-Prolog. This
+may not matter if you run tests locally and target your own particular
+platform. Other pack users may be running on a different operating
+system however and encounter issues when installing; especially so if
+your pack incorporates foreign libraries.
+
+## Usage
+
+Too long, won't read! In short, apply the following guidelines to your
+pack project repository on GitHub.
+
+Copy and paste the following YAML to your pack project at
+`.github/workflows/pack-cover.yaml` and commit the change. You will
+immediately see GitHub spool up runners for macOS, Windows and Ubuntu at
+the same time. After checking out your project repository, the runners
+will then proceed to install SWI-Prolog either using Homebrew,
+Chocolatey or APT depending on the runner's operating system. The pack
+will install, run tests with captured coverage statistics. You can see
+the normal SWI-Prolog coverage report in the runner job logs.
+
 ```yaml
 on:
   push:
@@ -19,5 +42,50 @@ jobs:
         os: [macOS, Windows, Ubuntu]
     steps:
       - uses: actions/checkout@v2
-      - uses: royratcliffe/swi-prolog-pack-cover
+      - uses: royratcliffe/swi-prolog-pack-cover@main
 ```
+
+Note that `uses` requires a repository _and_ repository branch, tag or
+commit reference; actions do _not_ default to the main branch.
+
+## Shield badges
+
+If you set up a Gist, your project can utilise
+[Shields](https://shields.io) to place coverage and failure badges on
+your project's README page. Set up two repository secrets for your
+actions; see variables below. Create a new Gist, a public one. Note its
+Gist identifier. You also need a GitHub personal-access token with
+_Create gists_ permissions.
+
+  * Variable `COVFAIL_GISTID` for the Gist identifier.
+  * Variable `GHAPI_PAT` for the private token.
+
+The action only updates the shield on *one* selected operating system,
+defaulting to Linux. Ubuntu Linux runs the development version of Prolog
+so the shield images reflect coverage performance using the latest
+odd-minor release. You also need to set up the environment for the
+coverage step.
+
+```yaml
+      - uses: royratcliffe/swi-prolog-pack-cover@main
+        env:
+          GHAPI_PAT: ${{ secrets.GHAPI_PAT }}
+          COVFAIL_GISTID: ${{ secrets.COVFAIL_GISTID }}
+```
+
+Add links to your README file as below, albeit after replacing the
+organisation and Gist identifier with your own.
+
+```markdown
+![cov](https://shields.io/endpoint?url=https://gist.githubusercontent.com/royratcliffe/ccccef2ac1329551794f2a466ee61014/raw/cov.json)
+![fail](https://shields.io/endpoint?url=https://gist.githubusercontent.com/royratcliffe/ccccef2ac1329551794f2a466ee61014/raw/fail.json)
+```
+
+## No symbolic linking
+
+Installing the pack involves building it using compiler tools if the
+pack includes foreign libraries. SWI-Prolog cleverly uses symbol links
+when installing a pack on those operating systems that support it;
+includes macOS and Linux. Important _not_ to link the pack. The default
+`link(true)` complicates coverage-by-file filtering because it compares
+source file paths relative to the installed pack directory.
--- action.pl
+++ action.pl
@@ -1,15 +1,59 @@
+/*  File:    action.pl
+    Author:  Roy Ratcliffe
+    Created: Feb 26 2022
+    Purpose: SWI-Prolog pack installation with test coverage
+
+Copyright (c) 2022, Roy Ratcliffe, United Kingdom
+
+Permission is hereby granted, free of charge,  to any person obtaining a
+copy  of  this  software  and    associated   documentation  files  (the
+"Software"), to deal in  the   Software  without  restriction, including
+without limitation the rights to  use,   copy,  modify,  merge, publish,
+distribute, sublicense, and/or sell  copies  of   the  Software,  and to
+permit persons to whom the Software is   furnished  to do so, subject to
+the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
+OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 :- use_module(library(aggregate), [aggregate_all/3]).
-:- use_module(library(apply), [convlist/3]).
+:- use_module(library(apply), [maplist/3, convlist/3, exclude/3]).
 :- use_module(library(filesex), [relative_file_name/3]).
 :- use_module(library(lists), [member/2]).
+:- use_module(library(option), [option/2]).
 :- use_module(library(ordsets),
-              [ ord_intersection/3,
-                ord_intersect/2,
-                ord_subtract/3
-              ]).
-:- use_module(library(plunit), [load_test_files/1]).
+              [ord_intersect/2, ord_intersection/3, ord_subtract/3]).
 :- use_module(library(prolog_pack), [pack_property/2]).
 :- use_module(library(test_cover), [show_coverage/1]).
+:- use_module(library(url), [parse_url/2]).
+:- use_module(library(http/http_client), [http_get/3]).
+:- use_module(library(http/json), [atom_json_term/3, json_write_dict/3]).
+:- use_module(library(plunit), [load_test_files/1]).
+:- use_module(library(settings), [setting/4, setting/2]).
+
+:- ensure_loaded(library(http/http_json)).
+
+:- setting(runner_os, atom, env('RUNNER_OS', ''),
+           'GitHub runner operating system').
+
+:- setting(shield_os, atom, env('SHIELD_OS', 'Linux'),
+           'Runner OS to use for updating shield Gist').
+
+:- setting(gist_id, atom, env('COVFAIL_GISTID', ''),
+           'Covered and failed-in-file Gist identifier').
+
+:- setting(access_token, atom, env('GHAPI_PAT', ''),
+           'GitHub API personal access token').
 
 :- initialization(main, main).
 
@@ -23,20 +67,65 @@ main :-
                      not_covered:NotCovered,
                      failed_in_file:FailedInFile
                  }, Covers),
-        all(SumInFile, SumNotCovered, SumFailedInFile, Count)),
+        all(SumInFile, SumNotCovered, SumFailedInFile, Count)
+    ),
     format('Clauses in files:~t~d~40|~n', [SumInFile]),
     format('Clauses not covered:~t~d~40|~n', [SumNotCovered]),
     format('Failed clauses in files:~t~d~40|~n', [SumFailedInFile]),
     format('Number of files:~t~d~40|~n', [Count]),
+    forall(member(Rel-Cover, Covers),
+           (   json_write_dict(
+                   current_output,
+                   _{  rel:Rel,
+                       cover:Cover
+                    }, [width(0)]
+               ),
+               nl
+           )),
     (   SumInFile > 0
     ->  NotCoveredPercent is 100 * SumNotCovered / SumInFile,
         FailedInFilePercent is 100 * SumFailedInFile / SumInFile,
         CoveredPercent is 100 - NotCoveredPercent,
         format('Not covered:~t~f~40|%~n', [NotCoveredPercent]),
         format('Failed in file:~t~f~40|%~n', [FailedInFilePercent]),
-        format('Covered:~t~f~40|%~n', [CoveredPercent])
+        format('Covered:~t~f~40|%~n', [CoveredPercent]),
+        shield(CoveredPercent, FailedInFilePercent)
     ;   true
     ).
+
+shield(Cov, Fail) :-
+    setting(runner_os, RunnerOS),
+    setting(shield_os, ShieldOS),
+    (   RunnerOS == ShieldOS
+    ->  shield(Cov, Fail, _)
+    ;   true
+    ).
+
+shield(Cov, Fail, Reply) :-
+    setting(gist_id, GistID),
+    GistID \== '',
+    !,
+    shield_files([cov-Cov, fail-Fail], Files),
+    ghapi_update_gist(GistID, json(json([files=Files])), Reply, []).
+
+shield_files(Pairs, json(Files)) :- maplist(shield_file, Pairs, Files).
+
+shield_file(Label-Percent, File=json([content=Content])) :-
+    atom_concat(Label, '.json', File),
+    format(atom(Message), '~1f%', [Percent]),
+    shield_color(Percent, Color),
+    atom_json_term(Content, json([ schemaVersion=1,
+                                   label=Label,
+                                   message=Message,
+                                   color=Color
+                                 ]), []),
+    format('raw/~s~n', [File]).
+
+shield_color(Percent, red) :- Percent < 20, !.
+shield_color(Percent, orange) :- Percent < 40, !.
+shield_color(Percent, yellow) :- Percent < 60, !.
+shield_color(Percent, yellowgreen) :- Percent < 80, !.
+shield_color(_, green).
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -109,9 +198,31 @@ file_cover(File, Succeeded, Failed,
     clean(NotCovered, NotCoveredLength),
     clean(FailedInFile, FailedInFileLength).
 
+%!  clean(Clauses, Length) is det.
+%
+%   Cleans the Clauses and counts the number of non-dirty clauses as
+%   Length where dirty includes:
+%
+%       - `user`, `plunit` or `prolog_cover` clauses;
+%       - 'unit test' head clauses.
+%
+%   The latter excludes the test heads that otherwise skew the total
+%   number of clauses upwards and therefore incorrectly reduces the
+%   coverage percentage.
+
 clean(Clauses, Length) :-
-    prolog_cover:clean_set(Clauses, CleanClauses),
+    prolog_cover:clean_set(Clauses, CleanClauses0),
+    exclude(is_dirty, CleanClauses0, CleanClauses),
     length(CleanClauses, Length).
+
+is_dirty(Clause) :-
+    clause_property(Clause, predicate(Predicate)),
+    dirty_predicate(Predicate).
+
+dirty_predicate(user:_/_) :- !.
+dirty_predicate(plunit:_/_) :- !.
+dirty_predicate(prolog_cover:_/_) :- !.
+dirty_predicate(_:'unit test'/_).
 
 %!  subdir(+Dir, +File, -Rel) is semidet.
 %
@@ -120,3 +231,76 @@ clean(Clauses, Length) :-
 subdir(Dir, File, Rel) :-
     relative_file_name(File, Dir, Rel),
     \+ sub_atom(Rel, 0, _, _, (..)).
+
+%!  ghapi_update_gist(+GistID, +Data, -Reply, +Options) is det.
+%
+%   Updates a Gist by its unique identifier. Data is the patch payload
+%   as a JSON object, or dictionary if you include json_object(dict) in
+%   Options. Reply is the updated Gist in JSON on success.
+%
+%   The example below illustrates a Gist update using a JSON term.
+%   Notice the doubly-nested `json/1` terms. The first sets up the HTTP
+%   request for JSON while the inner term specifies a JSON _object_
+%   payload. In this example, the update adds or replaces the `cov.json`
+%   file with content of "{}" as serialised JSON. Update requests for
+%   Gists have a `files` object with a nested filename-object comprising
+%   a content string for the new contents of the file.
+%
+%       ghapi_update_gist(
+%           ec92ac84832950815861d35c2f661953,
+%           json(json([ files=json([ 'cov.json'=json([ content='{}'
+%                                                    ])
+%                                  ])
+%                     ])), _, []).
+%
+%   @see https://docs.github.com/en/rest/reference/gists#update-a-gist
+
+ghapi_update_gist(GistID, Data, Reply, Options) :-
+    ghapi_get([gists, GistID], Reply, [method(patch), post(Data)|Options]).
+
+%!  ghapi_get(+PathComponents, +Data, +Options) is det.
+%
+%   Accesses the GitHub API. Supports JSON terms and dictionaries. For
+%   example, the following goal accesses the GitHub Gist API looking for
+%   a particular Gist by its identifier and unifies `A` with a JSON term
+%   representing the Gist's current contents and state.
+%
+%       ghapi_get([gists, ec92ac84832950815861d35c2f661953], A, []).
+%
+%   Supports all HTTP methods despite the predicate name. The "get"
+%   mirrors the underlying http_get/3 method which also supports all
+%   methods. POST and PATCH send data using the `post/1` option and
+%   override the default HTTP verb using the `method/1` option.
+%   Similarly here.
+%
+%   Handles authentication via settings, and from the system environment
+%   indirectly. Option `ghapi_access_token/1` overrides both. Order of
+%   overriding proceeds as: option, setting, environment, none. Empty
+%   atom counts as none.
+%
+%   Abstracts away the path using path components. Argument
+%   PathComponents is an atomic list specifying the URL path.
+
+ghapi_get(PathComponents, Data, Options) :-
+    ghapi_get_options(Options_, Options),
+    atomic_list_concat([''|PathComponents], /, Path),
+    parse_url(URL, [protocol(https), host('api.github.com'), path(Path)]),
+    http_get(URL, Data,
+             [ request_header('Accept'='application/vnd.github.v3+json')
+             | Options_
+             ]).
+
+ghapi_get_options([ request_header('Authorization'=Authorization)
+                  | Options
+                  ], Options) :-
+    ghapi_access_token(AccessToken, Options),
+    AccessToken \== '',
+    !,
+    format(atom(Authorization), 'token ~s', [AccessToken]).
+ghapi_get_options(Options, Options).
+
+ghapi_access_token(AccessToken, Options) :-
+    option(ghapi_access_token(AccessToken), Options),
+    !.
+ghapi_access_token(AccessToken, _Options) :-
+    setting(access_token, AccessToken).
