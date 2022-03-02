897415d2cabb3125c2802e75187e2f697a275602
Roy Ratcliffe <royratcliffe@me.com>
2022-03-02 08:22:22 +0000

    Shield images

--- action.pl
+++ action.pl
@@ -1,3 +1,31 @@
+/*  File:    action.pl
+    Author:  Roy Ratcliffe
+    Created: Feb 26 2022
+    Purpose: SWI-Prolog pack installation with test coverage
+
+Copyright (c) 2022, Roy Ratcliffe, United Kingdom
+
+Permission is hereby granted, free of charge,  to any person obtaining a
+copy  of  this  software  and    associated   documentation  files  (the
+"Software"), to deal in  the   Software  without  restriction, including
+without limitation the rights to  use,   copy,  modify,  merge, publish,
+distribute, sublicense, and/or sell  copies  of   the  Software,  and to
+permit persons to whom the Software is   furnished  to do so, subject to
+the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
+OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 :- use_module(library(aggregate), [aggregate_all/3]).
 :- use_module(library(apply), [convlist/3]).
 :- use_module(library(filesex), [relative_file_name/3]).
@@ -23,7 +51,8 @@ main :-
                      not_covered:NotCovered,
                      failed_in_file:FailedInFile
                  }, Covers),
-        all(SumInFile, SumNotCovered, SumFailedInFile, Count)),
+        all(SumInFile, SumNotCovered, SumFailedInFile, Count)
+    ),
     format('Clauses in files:~t~d~40|~n', [SumInFile]),
     format('Clauses not covered:~t~d~40|~n', [SumNotCovered]),
     format('Failed clauses in files:~t~d~40|~n', [SumFailedInFile]),
